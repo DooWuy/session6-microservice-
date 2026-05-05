4234cd3891ba9c2024568b67427ea096dbb305ad
huylebron <doquochuy27102003@gmail.com>
2026-05-05 22:59:35 +0700

    bài tập 1 - khá :  Thiết lập Eureka Server (Trạm điều hướng y tế)

---
+++ medical-discovery-server/src/main/java/medical/medicaldiscoveryserver/MedicalDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package medical.medicaldiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalDiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalDiscoveryServerApplication.class, args);
+    }
+
+}
---
+++ medical-discovery-server/src/test/java/medical/medicaldiscoveryserver/MedicalDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package medical.medicaldiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
